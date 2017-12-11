063cddb044b773a25f74f68098926737fe2cd027
Dennis Hodges <fermentationist@fermentnistsMBP.attlocal.net>
2017-12-10 22:17:57 -0600

    server working

--- server.js
+++ server.js
@@ -7,22 +7,19 @@ const handlebars = require("express-handlebars");
 // Set port
 const PORT = process.env.PORT || 3000;
 
-// Instantiate express server and routers
+// Instantiate express server
 const app = express();
-//const apiRouter = express.router();
-const htmlRouter = express.router();
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Middleware routers
-htmlRouter.use(express.static("public/"));
-//apiRouter.use("/api", apiRoutes);
+app.use(express.static("public/"));
+//app.use("/api", apiRoutes);
 
 
 // Start server
 app.listen(PORT, function(){
 	console.log("please pay no attention to port", PORT);
-	}
 });

45db6fec41075fcd169374b1739024fb81afacb3
Dennis Hoges <fermentationist@gmail.com>
2017-12-12 00:53:11 -0600

    added middleware to serve static resources

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 // Middleware routers
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+app.use(express.static("public"));
 
 // Start server
 app.listen(PORT, function(){

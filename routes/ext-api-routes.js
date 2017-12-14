3463d4f160d27df541aa41da5f7c27c66c1e243e
Dennis Hoges <fermentationist@gmail.com>
2017-12-14 02:31:01 -0600

    Glassdoor search working and able to send results back to front end (to the console)

---
+++ routes/ext-api-routes.js
@@ -0,0 +1,31 @@
+const ext_apiRoutes = (function(){
+	const express = require("express");
+	const path = require("path");
+	const bodyParser = require("body-parser");
+	const gd = require("../ext_api/glassdoor.js");
+	// const connection = require("../config/connection.js");
+
+	const router = express.Router();
+	router.use(bodyParser.urlencoded({extended: false}));
+	router.use(bodyParser.json());
+
+	router.post("/gd/search", function (req, res){
+		console.log("/gd/search");
+		let state = JSON.parse(Object.keys(req.body)[0]).state;
+		let city = JSON.parse(Object.keys(req.body)[0]).city;
+		let searchTerms = JSON.parse(Object.keys(req.body)[0]).employer;
+		var results = [];
+		gd.employersByCity(city, state, searchTerms, function (data){
+			data.employers.forEach(function (employer){
+				results.push([employer.name, employer.sectorName, employer.industryName, employer.overallRating]);
+			});
+			console.log("Employers", results);
+			res.json(results);
+		});
+	});
+
+
+	return router;
+})();
+console.log ("ext-api-routes.js loaded");
+module.exports = ext_apiRoutes;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const apiRoutes = require("./controllers/api-controller.js");
 const htmlRoutes = require("./routes/html-routes.js");
-
+const ext_apiRoutes = require("./routes/ext-api-routes.js");
 
 // Set port
 const PORT = process.env.PORT || 3000;
@@ -17,12 +17,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Middleware routers
-// app.use(function(req, res, next){
-// 	console.log("userAgent", req.get("User-Agent"))
-// 	next();
-// });
-
 app.use("/api", apiRoutes);
+app.use("/ext_api", ext_apiRoutes);
 app.use("/", htmlRoutes);
 app.use(express.static("public/"));
 

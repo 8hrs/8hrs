093cdd9ead73442670892f59c502fa6930bd3d64
Dennis Hoges <fermentationist@gmail.com>
2017-12-14 02:31:01 -0600

    Glassdoor search working and able to send results back to front end (to the console)

--- ext_api/glassdoor.js
+++ ext_api/glassdoor.js
@@ -1,7 +1,7 @@
 const Glassdoor = (function (){
 	// const bodyParser = require("body-parser");
 	const request = require("request");
-	function gdQuery (city = "", companyName = "", callback){
+	function gdQuery (city = "", state = "", searchTerms = "", callback){
 		let userIp;
 		request("https://api.ipify.org/?format=json", function (error, response, body){
 			userIp = JSON.parse(body).ip;
@@ -9,22 +9,22 @@ const Glassdoor = (function (){
 		})
 		const apiKey = process.env.GLASSDOOR_API_KEY;
 		const partnerId = process.env.GLASSDOOR_PARTNER_ID;
-		const pageNumber = 2;
-		const pageSize = 40;
-		let queryUrl = `http://api.glassdoor.com/api/api.htm?t.p=${partnerId}&t.k=${apiKey}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&userip=${userIp}&city=${city}&q=${companyName}&pn=${pageNumber}&ps=${pageSize}`;
+		const pageNumber = 1;
+		const pageSize = 100;
+		let queryUrl = `http://api.glassdoor.com/api/api.htm?t.p=${partnerId}&t.k=${apiKey}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&userip=${userIp}&city=${city}&q=${searchTerms}&pn=${pageNumber}&ps=${pageSize}`;
 		request(queryUrl, function (error, response, body){
 			if (error){
 				console.log(error.stack);
 			}
-			console.log(response);
+			// console.log(response);
 			let employers = JSON.parse(body).response;
-			console.log('employers', employers);
+			// console.log('employers', employers);
 			return callback(employers);
 		});
 	}
 
-	function employersByCity (city, companyName = "", callback){
-		return gdQuery(city, companyName, callback);
+	function employersByCity (city, state = "", searchTerms = "", callback){
+		return gdQuery(city, state, searchTerms, callback);
 	}
 	return {
 		// gdQuery: gdQuery,
@@ -32,12 +32,15 @@ const Glassdoor = (function (){
 	}
 })();
 
-Glassdoor.employersByCity("Chicago", "", function (res){
-    console.log('res', res.employers);
-	let employers = [];
-	res.employers.forEach(function (employer){
-		employers.push([employer.name, employer.sectorName, employer.industryName, employer.overallRating]);
-	});
-	console.log("\n\nby City : \n", employers);
-	return employers;
-});
+module.exports = Glassdoor;
+
+
+// Glassdoor.employersByCity("Chicago", "", function (res){
+//     console.log('res', res.employers);
+// 	let employers = [];
+// 	res.employers.forEach(function (employer){
+// 		employers.push([employer.name, employer.sectorName, employer.industryName, employer.overallRating]);
+// 	});
+// 	console.log("\n\nby City : \n", employers);
+// 	return employers;
+// });
--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,106 @@
 console.log("index.js loaded");
 
-const statesArray = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+const statesArray = ["AK - Alaska", 
+                "AL - Alabama", 
+                "AR - Arkansas", 
+                "AS - American Samoa", 
+                "AZ - Arizona", 
+                "CA - California", 
+                "CO - Colorado", 
+                "CT - Connecticut", 
+                "DC - District of Columbia", 
+                "DE - Delaware", 
+                "FL - Florida", 
+                "GA - Georgia", 
+                "GU - Guam", 
+                "HI - Hawaii", 
+                "IA - Iowa", 
+                "ID - Idaho", 
+                "IL - Illinois", 
+                "IN - Indiana", 
+                "KS - Kansas", 
+                "KY - Kentucky", 
+                "LA - Louisiana", 
+                "MA - Massachusetts", 
+                "MD - Maryland", 
+                "ME - Maine", 
+                "MI - Michigan", 
+                "MN - Minnesota", 
+                "MO - Missouri", 
+                "MS - Mississippi", 
+                "MT - Montana", 
+                "NC - North Carolina", 
+                "ND - North Dakota", 
+                "NE - Nebraska", 
+                "NH - New Hampshire", 
+                "NJ - New Jersey", 
+                "NM - New Mexico", 
+                "NV - Nevada", 
+                "NY - New York", 
+                "OH - Ohio", 
+                "OK - Oklahoma", 
+                "OR - Oregon", 
+                "PA - Pennsylvania", 
+                "PR - Puerto Rico", 
+                "RI - Rhode Island", 
+                "SC - South Carolina", 
+                "SD - South Dakota", 
+                "TN - Tennessee", 
+                "TX - Texas", 
+                "UT - Utah", 
+                "VA - Virginia", 
+                "VI - Virgin Islands", 
+                "VT - Vermont", 
+                "WA - Washington", 
+                "WI - Wisconsin", 
+                "WV - West Virginia", 
+                "WY - Wyoming"
+];
 
 $("#searchSubmit").on("click", function (){
 	console.log("clicked");
-	let employer = $("#employer").val();
+	let employer = $("#employer-name").val();
 	console.log('employer', employer);
 	let city  = $("#city").val();
 	console.log('city', city);
-	let state = $("#state").val();
+	let state = $("#state-dropdown").text().slice(5);
 	let searchObj = {
 		employer: employer,
 		city: city,
 		state: state
 	}
-	//searchCampaigns(searchObj)
+	console.log(searchObj);
+	searchGlassdoor(searchObj, function(result){
+		console.log(`this is the result being displayed by the callback function... ${result}`)
+	});
 });
 
-function searchCampaigns (searchObj, callback){
-	$.post("api/searchCampaigns", searchObj, function (){})
+
+function searchGlassdoor (searchObj, callback){
+	$.post("ext_api/gd/search", JSON.stringify(searchObj)).done(function (data){
+		console.log("sending post request");
+		console.log("* data = ", data);
+		return callback(data);
+	});
 }
 
 $(document).ready(function(){
-	createModalDropdown(statesArray);
+	createDropdown(statesArray);
 });
 
-function createModalDropdown (optionArray){
+function createDropdown (optionArray){
 	optionArray.forEach(function (option){
-		let line = $("<li role='presentation'>");
-		let anchor = $("<a role='menuitem'>").text(option);
+		let line = $("<li role='presentation'>").val(option);
+		let anchor = $("<a role='menuitem' class='stateOption'>").text(option);
 		line.append(anchor);
 		$("#states").append(line);
 	});
 }
+
+$(document).on("click", ".stateOption", function (event){
+	event.preventDefault();
+	$("#state-dropdown").text(this.innerHTML);
+	console.log(this.text + " !");
+});
+
 

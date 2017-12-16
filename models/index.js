2ca53d0df0ec5edfc25544de407fc808c8bb965c
Dennis Hodges <fermentationist@gmail.com>
2017-12-16 11:47:45 -0600

    troubleshooting connection in models/index.js

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV;// || 'development';
 if (env){
   console.log("NODE_ENV available");
+  console.log(NODE_ENV);
 }else{
   console.log("NODE_ENV not available");
 }

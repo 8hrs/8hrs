9ab227dfc78e59ee7cc8fe7d972783334b1ec7c0
Dennis Hoges <fermentationist@gmail.com>
2017-12-18 12:17:36 -0600

    finally, after some clumsy workarounds and ugly code, handlebars kind of works

--- ext_api/glassdoor.js
+++ ext_api/glassdoor.js
@@ -1,22 +1,23 @@
 const Glassdoor = (function (){
 	const request = require("request");
-	function employerQuery (city = "", state = "", searchTerms = "", callback){
+	function employerQuery (city, state, searchTerms, callback){
 		let userIp;
 		request("https://api.ipify.org/?format=json", function (error, response, body){
 			userIp = JSON.parse(body).ip;
-			console.log('userIp', userIp);
+			// console.log('userIp', userIp);
 		})
 		const apiKey = process.env.GLASSDOOR_API_KEY;
 		const partnerId = process.env.GLASSDOOR_PARTNER_ID;
-		const pageNumber = 1;
-		const pageSize = 100;
-		let queryUrl = `http://api.glassdoor.com/api/api.htm?t.p=${partnerId}&t.k=${apiKey}&format=json&v=1&action=employers&city=${city}&q=${searchTerms}&pn=${pageNumber}&ps=${pageSize}&employerId=10091`;
+		const pageNumber = "";
+		const pageSize = "";
+		let queryUrl = `http://api.glassdoor.com/api/api.htm?t.p=${partnerId}&t.k=${apiKey}&format=json&v=1&action=employers&city=${city}&q=${searchTerms}&pn=${pageNumber}&ps=${pageSize}`;
+		console.log('queryUrl', queryUrl);
 		request(queryUrl, function (error, response, body){
 			if (error){
 				console.log(error.stack);
 			}
 			let employers = JSON.parse(body).response;
-			console.log('response', response);
+			// console.log('response', response);
 			return callback(employers);
 		});
 	}
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,13 +9,15 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
+	const bodyParser = require("body-parser");
 
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.text());
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
+	app.get("/", function (req, res){
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	})
 
 };

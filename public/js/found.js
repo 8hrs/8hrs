de4e057d18c6a85118736e2a616a4ebcd116273b
Dennis Hodges <fermentationist@gmail.com>
2018-01-06 12:11:52 -0600

    ?

--- public/js/found.js
+++ public/js/found.js
@@ -8,14 +8,15 @@ function join() {
 	let formData = {};
 	formData.userName = $("#submitUser").val().trim();
 	formData.email = $("#submitEmail").val().trim();
-	console.log('formData.email', formData.email);
-	// upsertUser(formData, function(){
+	console.log('formData', formData);
+	upsertUser(formData, function(){
 		alert("Thanks for joining!");
-		return location = "/";
-	// })      
+		return //location = "/";
+	})      
 }
 
 function upsertUser(formData, callback) {
+    console.log('formData', formData);
         $.post("/users", formData).done(function(){
             return callback();
         }).fail(function(){

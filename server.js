181bdcf23b9ad50c7fe3beb22e736c81c082e4d7
Dennis Hoges <fermentationist@gmail.com>
2018-01-10 11:32:44 -0600

    cleanup

--- server.js
+++ server.js
@@ -8,27 +8,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const exhbs = require("express-handlebars");
+
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
+// =============================================================
 const db = require("./models");
 
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Handlebars
+// =============================================================
 const hbs = exhbs.create({defaultLayout: "main"})
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", "./views/");
-
-
 
 // Routes
 // =============================================================
@@ -39,6 +40,7 @@ require("./controllers/html-routes.js")(app);
 console.log("app info:", app.__router);
 
 // Static directory
+// =============================================================
 app.use(express.static("public"));
 
 // Syncing our sequelize models and then starting our Express app
